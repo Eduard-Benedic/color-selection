5aa97e41449160433b4de0df0a30a9f0d76784d5
Eduard Benedic <benediceduard@outlook.com>
2020-05-07 09:58:07 +0100

    config vars

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 module.exports = {
-  getDbConnectionString: function() {
+  getDbConnectionString: function () {
     // return `mongodb+srv://${config.username}:${config.password}@cluster0-snmef.mongodb.net/clarifai-colors`;
     return `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-snmef.mongodb.net/${process.env.DB_NAME}`;
   },
